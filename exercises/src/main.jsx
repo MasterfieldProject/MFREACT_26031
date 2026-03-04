4a4b339c00bbcca8e0f7ba8b74638880d76feed8
master <gbujdoso>
2026-03-04 09:27:52 +0100

    v2

--- exercises/src/main.jsx
+++ exercises/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './Exercise10.jsx'
+import App from './Exercise16.jsx'
 //import App from './App.jsx'
 import './Exercise1.js'
 
