8639fdb3b4e502be33d034bb6df220695cfc6bca
master <gbujdoso>
2026-03-09 15:43:43 +0100

    Add SpeedLimit component with speed range input and conditional image display
    - Created a new React component `SpeedLimit` in `Exercise3.jsx`
    - Implemented a state to track the current speed
    - Added a range input to adjust the speed between 0 and 200 km/h
    - Displayed an image when the speed exceeds 50 km/h

---
+++ exercises/src/Exercise3.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import table50 from "./50_kph.png";
+
+export default class SpeedLimit extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            speed: 20
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({ speed: event.target.value });
+    }
+
+    render() {
+        return (
+            <>
+                <div>
+                    <h1>Sebessége: {this.state.speed} km/h</h1>
+                    <input type="range" min="0" max="200" value={this.state.speed}
+                        onChange={this.handleChange} />
+                </div>
+                {this.state.speed > 50 && <img src={table50} alt="speed limit exceeded" width="50" />}
+            </>
+        )
+    }
+}
--- exercises/src/main.jsx
+++ exercises/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
-import App from './Counter.jsx'
+import App from './Exercise3.jsx'
 import './Exercise1.js'
 
 createRoot(document.getElementById('root')).render(
