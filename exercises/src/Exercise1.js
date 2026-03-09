260b9416b74305284f2fbd45e790e80430d2469d
master <gbujdoso>
2026-03-09 10:36:01 +0100

    v1.2

--- exercises/src/Exercise1.js
+++ exercises/src/Exercise1.js
@@ -1,7 +1,16 @@
 let unused = 10;
 
-const getWeather = function () {
-    console.log("Ez az utasítás függvényen belül van");
-};
-console.log("Ez az utasítás függvényen kívül van");
-getWeather();
+function getWeather(day, weather = "esős", ...moreArgs) {
+    console.log("Ma , " + day + " " + weather + " idő van.");
+    for (let i = 0; i < moreArgs.length; i++) {
+        console.log(moreArgs[i]);
+    }
+}
+getWeather("hétfőn");
+
+function print(nameFunction) {
+    return ("Hello " + nameFunction() + ".");
+}
+console.log(print(() => "Masterfield"));
+
+
