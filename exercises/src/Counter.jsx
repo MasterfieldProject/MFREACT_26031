2d22bd063256c94966965039767a1d8f5d45fbc8
master <gbujdoso>
2026-03-09 15:31:55 +0100

    Enhance Counter component to use flushSync for state updates and add hasButtonClicked state

--- exercises/src/Counter.jsx
+++ exercises/src/Counter.jsx
@@ -1,16 +1,62 @@
 import React from 'react';
+import { flushSync } from 'react-dom';
 
 export default class Counter extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            cnt: 0
+            cnt: 0,
+            hasButtonClicked: false
         }
     }
 
     increment = () => {
-        this.setState((prevState) => ({ cnt: prevState.cnt + 1 }));
+        /* jo megoldas */
+        /*
+        this.setState((prevState) => ({ cnt: prevState.cnt + 1 }), () => {
+            // callback function, called after the state update is done
+            this.setState({ hasButtonClicked: this.state.cnt > 0 })
+        })
+            */
+
+        /*
+        for (let i = 0; i < 5; i++) {
+            // this.setState({ cnt: this.state.cnt + 1 }); // rossz
+            this.setState((prevState) => ({ cnt: prevState.cnt + 1 })); // updater function, prevState: a state snapshot before the update
+            console.log(this.state.cnt);
+        }
+        */
+
+        // szinkronizálhatók a setState hivasok
+        for (let i = 0; i < 5; i++) {
+            flushSync(() => {
+                this.setState({ cnt: this.state.cnt + 1 });
+            });
+            console.log(this.state.cnt);
+        }
+
+        //this.setState((prevState) => ({ cnt: prevState.cnt + 1 })); // updater function, prevState: a state snapshot before the update
+
+        /* egymastol fuggo valtozok eseten hibas, ha egy setSTate-ben modositom */
+        /*
+        this.setState({ cnt: this.state.cnt + 1, hasButtonClicked: this.state.cnt > 0 })
+        */
+
+        /* egymastol fuggo valtozok eseten hibas a tobb setState hivas */
+        /*
+        this.setState((prevState) => ({ cnt: prevState.cnt + 1 })); // updater function, prevState: a state snapshot before the update
+        this.setState({ hasButtonClicked: this.state.cnt > 0 });
+        */
+
+
+
+        /* ilyet nem csinalunk, helyette a setState metódust használjuk*/
+        /*
+        this.state.cnt = this.state.cnt + 1;
+        console.log(this.state.cnt);
+        */
+
     }
 
     decrement = () => {
@@ -22,6 +68,7 @@ export default class Counter extends React.Component {
         return (
             <div>
                 <h1>Counter = {this.state.cnt}</h1>
+                <h2>hasButtonClicked = {'' + this.state.hasButtonClicked}</h2>
                 <button onClick={this.increment} >INC</button>
                 <button onClick={this.decrement} >DEC</button>
             </div>
