d3be367c5b37ff7fa6ac35f6caa16a332bf97011
master <gbujdoso>
2026-03-09 07:58:38 +0100

    v1

--- exercises/src/main.jsx
+++ exercises/src/main.jsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './Exercise16.jsx'
-//import App from './App.jsx'
-import './Exercise1.js'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
