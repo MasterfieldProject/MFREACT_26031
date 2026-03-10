9cd920193a87dfdf597c60d9e74bd3a9e90b128c
master <gbujdoso>
2026-03-10 15:47:33 +0100

    Add county selection dropdown with default value to form

--- exercises/src/Exercise10.jsx
+++ exercises/src/Exercise10.jsx
@@ -13,7 +13,7 @@ export default function Form() {
         birthDate: null,
         taxnum: '',
         postcode: '',
-        county: '',
+        county: 'Pest megye',
         errormsg: '',
         validtaxnum: false
     });
@@ -151,6 +151,32 @@ export default function Form() {
                 <label>
                     Irányítószám<br />
                     <input type="text" name="postcode" value={formData.postcode} onChange={handleChangePostcode} maxLength="4" />
+                </label>
+                <br />
+                <label>
+                    Megye<br />
+                    <select name="county" value={formData.county} onChange={handleChange} >
+                        <option value="Budapest">Budapest</option>
+                        <option value="Bács-Kiskun megye">Bács-Kiskun megye</option>
+                        <option value="Baranya megye">Baranya megye</option>
+                        <option value="Békés megye">Békés megye</option>
+                        <option value="Borsod-Abaúj-Zemplén megye">Borsod-Abaúj-Zemplén megye</option>
+                        <option value="Csongrád megye">Csongrád megye</option>
+                        <option value="Fejér megye">Fejér megye</option>
+                        <option value="Győr-Moson-Sopron megye">Győr-Moson-Sopron megye</option>
+                        <option value="Hajdú-Bihar megye">Hajdú-Bihar megye</option>
+                        <option value="Heves megye">Heves megye</option>
+                        <option value="Jász-Nagykun-Szolnok megye">Jász-Nagykun-Szolnok megye</option>
+                        <option value="Komárom-Esztergom megye">Komárom-Esztergom megye</option>
+                        <option value="Nógrád megye">Nógrád megye</option>
+                        <option value="Pest megye">Pest megye</option>
+                        <option value="Somogy megye">Somogy megye</option>
+                        <option value="Szabolcs-Szatmár-Bereg megye">Szabolcs-Szatmár-Bereg megye</option>
+                        <option value="Tolna megye">Tolna megye</option>
+                        <option value="Vas megye">Vas megye</option>
+                        <option value="Veszprém megye">Veszprém megye</option>
+                        <option value="Zala megye">Zala megye</option>
+                    </select>
                 </label>
                 <br />
                 <br />
