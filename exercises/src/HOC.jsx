3189538afc7f2654d3f23894b44bd1c0d74c4aca
master <gbujdoso>
2026-03-11 15:49:28 +0100

    v26

---
+++ exercises/src/HOC.jsx
@@ -0,0 +1,31 @@
+
+function BaseComponent(props) {
+  return (
+    <div onClick={props.onClick}>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+    </div>
+  );
+}
+
+const withLogger = (WrappedComponent) => {
+  return function ClickLogger(props) {
+
+    function onClick(e) {
+      console.log(e)
+    }
+
+    const { title, content } = props;
+    return (
+      <div>
+        <WrappedComponent {...props} onClick={onClick} />
+      </div>
+    );
+  }
+}
+
+const WrappedComp = withLogger(BaseComponent);
+
+export default function App() {
+  return <WrappedComp title="Masterfield" content="Szöveg" />
+}
